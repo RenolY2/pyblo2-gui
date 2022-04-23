48f15bafda2556d0958efe826ab658c0b4809dc7
Yoshi2 <debugyoshi@gmail.com>
2022-04-23 14:49:59 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 import os 
 from cx_Freeze import setup, Executable
-version = "0.1.2.2"
+version = "0.1.2.3"
 # Dependencies are automatically detected, but it might need fine tuning.
 
 def files_from_folder(folder):
