d00ca133643c8c92545f5fef296fd6171f1bd48e
Yoshi2 <debugyoshi@gmail.com>
2022-06-03 16:41:43 +0200

    forgot to push this

---
+++ widgets/texture_handler_widget.py
@@ -0,0 +1,35 @@
+from PyQt5.QtWidgets import QMenu, QAction, QFileDialog
+from PyQt5.QtCore import pyqtSignal
+from lib.blo.tex.textures import TextureHandler
+
+
+class TextureHandlerMenu(QMenu):
+    filter_update = pyqtSignal()
+
+    def __init__(self, editor, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.editor = editor
+        self.texture_handler = TextureHandler()
+
+        self.setTitle("Textures")
+
+        self.load_folder_action = QAction("Load From Folder", self)
+        self.load_folder_action.triggered.connect(self.load_folder)
+        self.addAction(self.load_folder_action)
+
+        self.load_archive_action = QAction("Load From Archive", self)
+        self.load_archive_action.triggered.connect(self.load_archive)
+        self.addAction(self.load_archive_action)
+
+    def load_folder(self):
+        filepath = QFileDialog.getExistingDirectory(
+            self, "Choose Texture Folder",
+            "")
+
+        self.texture_handler.init_from_folder(filepath)
+
+    def load_archive(self):
+        filepath, choosentype = QFileDialog.getOpenFileName(
+            self, "Open File",
+            "",
+            "Archive (*.arc, *.szs);;All files (*)")
