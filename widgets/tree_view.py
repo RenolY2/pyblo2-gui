e370de1df34f3fb588391fbc26692bec946451e2
Yoshi2 <debugyoshi@gmail.com>
2022-06-21 23:41:57 +0200

    clean up

--- widgets/tree_view.py
+++ widgets/tree_view.py
@@ -130,8 +130,6 @@ class PaneItem(NamedItemWithChildren):
         self.setText(0, name)
 
 
-
-
 class TextboxItem(PaneItem):
     pass
 
@@ -142,98 +140,6 @@ class PictureItem(PaneItem):
 
 class WindowItem(PaneItem):
     pass
-
-
-
-"""
-class EnemyRoutePoint(NamedItem):
-    def update_name(self):
-        group_item = self.parent()
-        group = group_item.bound_to
-
-        index = group.points.index(self.bound_to)
-
-        self.setText(0, "Enemy Route Point {0}".format(index))
-
-
-class Checkpoint(NamedItem):
-    def update_name(self):
-        offset = 0
-        group_item = self.parent()
-        groups_item = group_item.parent()
-        for i in range(groups_item.childCount()):
-            other_group_item = groups_item.child(i)
-            if other_group_item == group_item:
-                break
-            else:
-                print("Hmmm,",other_group_item.text(0), len(other_group_item.bound_to.points), offset)
-                group_object = other_group_item.bound_to
-                offset += len(group_object.points)
-
-        group = group_item.bound_to
-
-        index = group.points.index(self.bound_to)
-
-        self.setText(0, "Checkpoint {0} (pos={1})".format(index+1+offset, index))
-
-
-class ObjectRoutePoint(NamedItem):
-    def update_name(self):
-        group_item = self.parent()
-        group = group_item.bound_to
-
-        index = group.points.index(self.bound_to)
-
-        self.setText(0, "Object Route Point {0}".format(index))
-
-
-class ObjectEntry(NamedItem):
-    def __init__(self, parent, name, bound_to):
-        super().__init__(parent, name, bound_to)
-        bound_to.widget = self
-
-    def update_name(self):
-        self.setText(0, get_full_name(self.bound_to.objectid))
-
-    def __lt__(self, other):
-        return self.bound_to.objectid < other.bound_to.objectid
-
-
-class KartpointEntry(NamedItem):
-    def update_name(self):
-        playerid = self.bound_to.playerid
-        if playerid == 0xFF:
-            result = "All"
-        else:
-            result = "ID:{0}".format(playerid)
-        self.setText(0, "Kart Start Point {0}".format(result))
-
-
-class AreaEntry(NamedItem):
-    def update_name(self):
-        self.setText(0, "Area (Type: {0})".format(self.bound_to.area_type))
-
-
-class CameraEntry(NamedItem):
-    def update_name(self):
-        self.setText(0, "Camera {0} (Type: {1})".format(self.index, self.bound_to.camtype))
-
-
-class RespawnEntry(NamedItem):
-    def update_name(self):
-        self.setText(0, "Respawn Point (ID: {0})".format(self.bound_to.respawn_id))
-
-
-class LightParamEntry(NamedItem):
-    def update_name(self):
-        self.setText(0, "LightParam {0}".format(self.index))
-
-
-class MGEntry(NamedItem):
-    def update_name(self):
-        self.setText(0, "MG")
-"""
-
 
 
 class LayoutDataTreeView(QTreeWidget):
@@ -270,17 +176,6 @@ class LayoutDataTreeView(QTreeWidget):
         for item in (self.texture_list, self.material_list, self.font_list, self.layout):
             self.addTopLevelItem(item)
 
-        """self.enemyroutes = self._add_group("Enemy point groups")
-        self.checkpointgroups = self._add_group("Checkpoint groups")
-        self.objectroutes = self._add_group("Object point groups")
-        self.objects = self._add_group("Objects", ObjectGroupObjects)
-        self.kartpoints = self._add_group("Kart start points")
-        self.areas = self._add_group("Areas")
-        self.cameras = self._add_group("Cameras")
-        self.respawnpoints = self._add_group("Respawn points")
-        self.lightparams = self._add_group("Light param entries")
-        self.mgentries = self._add_group("MG entries")
-        """
         self.setContextMenuPolicy(Qt.CustomContextMenu)
         self.customContextMenuRequested.connect(self.run_context_menu)
 
@@ -359,18 +254,6 @@ class LayoutDataTreeView(QTreeWidget):
                 blo_item.parent = new_blo_item
             self.rebuild_tree.emit()
             e.acceptProposedAction()
-        """ 
-        print(e.dropAction())
-        # super(Tree, self).dropEvent(e)
-        index = self.indexAt(e.pos())
-        parent = index.parent()
-        print('in', index.row())
-        print(e.mimeData().text())
-        print(e.mimeData().formats())
-        print(e.mimeData().data('application/x-qabstractitemmodeldatalist'))
-        text = e.mimeData().data('application/x-qabstractitemmodeldatalist')
-        self.model().dropMimeData(e.mimeData(), e.dropAction(), index.row(), index.column(), parent)"""
-        #e.reject()
 
     def emit_add_item(self, pos):
         item = self.itemAt(pos)
@@ -473,7 +356,6 @@ class LayoutDataTreeView(QTreeWidget):
                     error = ("Cannot delete, texture is used by following material{1}: \n"
                              "{0}".format(", ".join(result), ending))
                 open_error_dialog(error, self)
-
 
     def handle_delete(self, pos):
         item = self.itemAt(pos)
@@ -677,48 +559,6 @@ class LayoutDataTreeView(QTreeWidget):
             texture_item = Texture(self.texture_list, texture, texture, None)
 
         self.set_node_objects(screen_data.root, self.layout)
-
-        """
-        for group in boldata.enemypointgroups.groups:
-            group_item = EnemyPointGroup(self.enemyroutes, group)
-
-            for point in group.points:
-                point_item = EnemyRoutePoint(group_item, "Enemy Route Point", point)
-
-        for group in boldata.checkpoints.groups:
-            group_item = CheckpointGroup(self.checkpointgroups, group)
-
-            for point in group.points:
-                point_item = Checkpoint(group_item, "Checkpoint", point)
-
-        for route in boldata.routes:
-            route_item = ObjectPointGroup(self.objectroutes, route)
-
-            for point in route.points:
-                point_item = ObjectRoutePoint(route_item, "Object route point", point)
-
-        for object in boldata.objects.objects:
-            object_item = ObjectEntry(self.objects, "Object", object)
-
-        self.sort_objects()"""
-
-        """for kartpoint in boldata.kartpoints.positions:
-            item = KartpointEntry(self.kartpoints, "Kartpoint", kartpoint)
-
-        for area in boldata.areas.areas:
-            item = AreaEntry(self.areas, "Area", area)
-
-        for respawn in boldata.respawnpoints:
-            item = RespawnEntry(self.respawnpoints, "Respawn", respawn)
-
-        for i, camera in enumerate(boldata.cameras):
-            item = CameraEntry(self.cameras, "Camera", camera, i)
-
-        for i, lightparam in enumerate(boldata.lightparams):
-            item = LightParamEntry(self.lightparams, "LightParam", lightparam, i)
-
-        for mg in boldata.mgentries:
-            item = MGEntry(self.mgentries, "MG", mg)"""
 
     def save_expand_status(self, expand, child: NamedItemWithChildren):
         if child.isExpanded():
